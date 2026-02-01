8088caa8c93a1fd024e131317d6212aae09ec00f
Wix Remote Machine <support@wix.com>
2026-02-01 11:42:07 +0000

    Create New Home Page

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider, Navigate, Outlet } from 'react-rou
 import { ScrollToTop } from '@/lib/scroll-to-top';
 import ErrorPage from '@/integrations/errorHandlers/ErrorPage';
 import HomePage from '@/components/pages/HomePage';
+import ProductLandingPage from '@/components/pages/ProductLandingPage';
 
 // Layout component that includes ScrollToTop
 function Layout() {
@@ -25,6 +26,13 @@ const router = createBrowserRouter([
         element: <HomePage />,
         routeMetadata: {
           pageIdentifier: 'home',
+        },
+      },
+      {
+        path: "product",
+        element: <ProductLandingPage />,
+        routeMetadata: {
+          pageIdentifier: 'product',
         },
       },
       {
