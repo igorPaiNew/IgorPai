5b13c9bbc0d4ccd6ed88a51d992a238185c9d254
Wix Remote Machine <support@wix.com>
2026-02-02 15:48:32 +0000

    Change website title

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,6 +1,7 @@
 export const Head = () => {
   return (
     <>
+      <title>IgorPai</title>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       {/* Fonts */}
