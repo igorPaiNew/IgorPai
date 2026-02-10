7061d118b475b8275e990c0f4166e4c49f929089
Wix Remote Machine <support@wix.com>
2026-02-10 14:49:38 +0000

    Fix Cart page loading

--- src/components/ecom/Cart.tsx
+++ src/components/ecom/Cart.tsx
@@ -59,7 +59,7 @@ export default function CartContent() {
       </div>
 
       {/* Cart with Items */}
-      <CartLineItems emptyState={emptyCartState}>
+      <CartLineItems emptyState={EmptyCartState}>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           {/* Cart Items */}
           <div className="lg:col-span-2">
@@ -189,7 +189,7 @@ export default function CartContent() {
   );
 }
 
-const emptyCartState = () => {
+const EmptyCartState = () => {
   const Navigation = useNavigation();
 
   return (
